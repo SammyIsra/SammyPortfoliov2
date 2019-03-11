d888bca0c0980dba69cab7a804ac5bcd72ef9607
SammyIsra <sammy.israwi@gmail.com>
2019-03-11 00:10:00 -0400

    Ran prettier

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -14,22 +14,37 @@ const FooterIconItem = styled.div`
 `;
 
 const FooterContainer = styled.div`
-  background-color: ${props => props.dark ? "rgb(51,51,51)" : "white" };
-  color: ${props => props.dark? "white" : "inherit"};
+  background-color: ${props => (props.dark ? "rgb(51,51,51)" : "white")};
+  color: ${props => (props.dark ? "white" : "inherit")};
   padding: 1.5rem;
 `;
 
-export function Footer({dark}){
+export function Footer({ dark }) {
   return (
     <FooterContainer dark={dark}>
-      Made proudly by Sammy: Finding your lack of sense of pride and accomplishment, disturbing.
+      Made proudly by Sammy: Finding your lack of sense of pride and
+      accomplishment, disturbing.
       <FooterIconsContainer>
-        <FooterIconItem><a href="https://twitter.com/SammyIs_Me">Twitter</a></FooterIconItem>      
-        <FooterIconItem><a href="https://www.flickr.com/photos/sammy_iy/">Flickr</a></FooterIconItem>
-        <FooterIconItem><a href="https://www.linkedin.com/in/sammyisrawi/">LinkedIn</a></FooterIconItem>
-        <FooterIconItem><a href="https://dev.to/sammyisa">Dev.to</a></FooterIconItem>
-        <FooterIconItem><a href="https://github.com/SammyIsra">Github</a></FooterIconItem>
-        <FooterIconItem><a href="https://drive.google.com/file/d/0B1ouby1hW-sjSDdZSFYtUFB1TVE/view?usp=sharing">Resume</a></FooterIconItem>
+        <FooterIconItem>
+          <a href="https://twitter.com/SammyIs_Me">Twitter</a>
+        </FooterIconItem>
+        <FooterIconItem>
+          <a href="https://www.flickr.com/photos/sammy_iy/">Flickr</a>
+        </FooterIconItem>
+        <FooterIconItem>
+          <a href="https://www.linkedin.com/in/sammyisrawi/">LinkedIn</a>
+        </FooterIconItem>
+        <FooterIconItem>
+          <a href="https://dev.to/sammyisa">Dev.to</a>
+        </FooterIconItem>
+        <FooterIconItem>
+          <a href="https://github.com/SammyIsra">Github</a>
+        </FooterIconItem>
+        <FooterIconItem>
+          <a href="https://drive.google.com/file/d/0B1ouby1hW-sjSDdZSFYtUFB1TVE/view?usp=sharing">
+            Resume
+          </a>
+        </FooterIconItem>
       </FooterIconsContainer>
     </FooterContainer>
   );
